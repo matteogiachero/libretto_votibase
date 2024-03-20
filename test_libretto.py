71a286f9b939e198d9157d553a23afc035a505d8
matte <s280956@studenti.polito.it>
2024-03-20 13:17:04 +0100

    Lavoro in classe finito 18/03/24

--- test_libretto.py
+++ test_libretto.py
@@ -1,29 +1,52 @@
 from voto import Libretto, Voto
 
-lib = Libretto ()
+lib = Libretto()
 
-v1 = Voto("Analisi 1", 10, 28, False, "2022-01-30")
+v1 = Voto("Analisi I", 10, 28, False, '2022-01-30')
 lib.append(v1)
-lib.append(Voto("Fisica 1", 10, 25, False, "2022-07-12"))
-lib.append(Voto("Analisi 2", 8, 30, True, "2023-02-15"))
 
-voti25 = (lib.findByPunteggio(25, False))
+lib.append(Voto("Fisica I", 10, 25, False, '2022-07-12'))
+lib.append(Voto("Analisi II", 8, 30, True, '2023-02-15'))
 
+voti25 = lib.findByPunteggio(25, False)
 for v in voti25:
     print(v.esame)
 
-voto_analisi2 = lib.findByEsame("Analisi 2")
+voto_analisi2 = lib.findByEsame("Analisi III")
 if voto_analisi2 is None:
     print("Nessun voto trovato")
 else:
-    print (f"Hai preso {voto_analisi2.str_punteggio()}")
+    print(f'Hai preso {voto_analisi2.str_punteggio()}')
+
 try:
-    voto_analisi2 = lib.findByEsame2("Analisi 3")
-    print(f"Hai preso {voto_analisi2.str_punteggio}")
+    voto_analisi2 = lib.findByEsame2("Analisi III")
+    print(f'Hai preso {voto_analisi2.str_punteggio()}')
 except ValueError:
     print("Nessun voto trovato")
-nuovo1 = Voto("Fisica 1", 10, 25, False, "2022-07-13")
-nuovo2 = Voto("Fisica 2", 10, 25, False, "2022-07-13")
+
+nuovo1 = Voto("Fisica I", 10, 25, False, '2022-07-13')
+nuovo2 = Voto("Fisica II", 10, 25, False, '2022-07-13')
 print("1)", lib.has_voto(nuovo1))
 print("2)", lib.has_voto(nuovo2))
 
+lib.append(Voto("Analisi 1", 10, 18,False, '2020-01-01'))
+lib.append(Voto("Chimica", 8, 30,False, '2020-01-02'))
+lib.append(Voto("Informatica", 8, 30,True, '2020-01-03'))
+lib.append(Voto("Algebra Lineare", 10, 24,False, '2020-06-01'))
+lib.append(Voto("Fisica 1", 10, 21,False, '2020-06-02'))
+
+migliorato = lib.crea_migliorato()
+lib.append(Voto("Tesi", 3, 0,False, '2020-06-01'))
+
+print("Libretto originario")
+lib.stampa()
+print("Libretto migliorato")
+migliorato.stampa()
+
+ordinato = lib.crea_ordinato_per_esame()
+print("Libretto ordinato per esame")
+ordinato.stampa()
+
+ordinato.cancella_inferiori(24)
+print("Libretto senza i voti brutti")
+ordinato.stampa()
--- voto.py
+++ voto.py
@@ -1,6 +1,10 @@
-from dataclasses import dataclass
+# from dataclasses import dataclass
 
-@dataclass
+import dataclasses
+import operator
+
+
+@dataclasses.dataclass
 class Voto:
     esame: str
     cfu: int
@@ -10,36 +14,49 @@ class Voto:
 
     def str_punteggio(self):
         """
-        Costruisce la stringa che rappresenta in forma leggibile il punteggio, tenendo conto della possibile lode
-        :return: "30 e lode" oppure il punteggio, sotto forma di stringa
+        Costruisce la stringa che rappresenta in forma leggibile il punteggio,
+        tenendo conto della possibilità di lode
+        :return: "30 e lode" oppure il punteggio (senza lode), sotto forma di stringa
         """
         if self.punteggio == 30 and self.lode:
             return "30 e lode"
         else:
             return f"{self.punteggio}"
+            # return self.punteggio  NOOO
 
+    def copy(self):
+        return Voto(self.esame, self.cfu, self.punteggio, self.lode, self.data)
+
+    def __str__(self):
+        return f'{self.esame} ({self.cfu} CFU): voto {self.str_punteggio()} il {self.data}'
+
+
+def estrai_campo_esame(v):
+    return v.esame
 
 class Libretto:
     def __init__(self):
         self._voti = []
 
     def append(self, voto):
-        if self.has_voto(voto)== False and self.has_conflitto(voto)== False:
+        if self.has_voto(voto)==False and self.has_conflitto(voto)==False:
             self._voti.append(voto)
         else:
             raise ValueError("Voto non valido")
 
     def media(self):
-        if len(self._voti) == 0:
+        if len(self._voti)==0:
             raise ValueError("Elenco voti vuoto")
-        punteggi = [v.punteggio for v in self._voti]
-        return sum(punteggi)/len(punteggi)
+        punteggi = [v.punteggio * v.cfu for v in self._voti]
+        cfu = [v.cfu for v in self._voti]
+        return sum(punteggi)/sum(cfu)
+
     def findByPunteggio(self, punteggio, lode):
         """
-        Seleziona i soli voti che hanno un punteggio definito
+        Seleziona tutti e soli i soli voti che hanno un punteggio definito.
         :param punteggio: numero intero che rappresenta il punteggio
         :param lode: booleano che indica la presenza della lode
-        :return: lista di oggetti di tipo voto che hanno il punteggio specificato
+        :return: lista di oggetti di tipo Voto che hanno il punteggio specificato (può anche essere vuota)
         """
         corsi = []
         for v in self._voti:
@@ -49,9 +66,9 @@ class Libretto:
 
     def findByEsame(self, esame):
         """
-        Cerca il voto a partire dal nome dell'esame
-        :param esame: Nome dell'esame da cercare
-        :return: oggetto Voto corrispondente al nome trovato oppure None se non viene trovato
+        Cerca il voto a partire dal nome dell'esame.
+        :param esame: Nome dall'esame da ricercare
+        :return: l'oggetto Voto corrispondente al nome trovato, oppure None se non viene trovato
         """
         for v in self._voti:
             if v.esame == esame:
@@ -60,35 +77,139 @@ class Libretto:
 
     def findByEsame2(self, esame):
         """
-        Cerca il voto a partire dal nome dell'esame
-        :param esame: Nome dell'esame da cercare
-        :return: oggetto Voto corrispondente al nome trovato oppure un'eccezione ValueError
+        Cerca il voto a partire dal nome dell'esame.
+        :param esame: Nome dall'esame da ricercare
+        :return: l'oggetto Voto corrispondente al nome trovato, oppure un'eccezione ValueError se
+        l'elemento non viene trovato
         """
         for v in self._voti:
             if v.esame == esame:
                 return v
-        raise ValueError
+        raise ValueError(f"Esame '{esame}' non presente nel libretto")
+
 
     def has_voto(self, voto):
         """
         Ricerca se nel libretto esiste già un esame con lo stesso nome e lo stesso punteggio
-        :param voto: oggetto voto da confrontare
+        :param voto: oggetto Voto da confrontare
         :return: True se esiste, False se non esiste
         """
         for v in self._voti:
             if v.esame == voto.esame and v.punteggio == voto.punteggio and v.lode == voto.lode:
                 return True
         return False
+
     def has_conflitto(self, voto):
         """
         Ricerca se nel libretto esiste già un esame con lo stesso nome ma punteggio diverso
-        :param voto: oggetto voto da confrontare
+        :param voto: oggetto Voto da confrontare
         :return: True se esiste, False se non esiste
         """
         for v in self._voti:
-            if v.esame == voto.esame and not (v.punteggio == voto.punteggio and v.lode == voto.lode):
-                return True
+            if v.esame == voto.esame and not(v.punteggio == voto.punteggio and v.lode == voto.lode):
+            # if v.esame == voto.esame and (v.punteggio != voto.punteggio or v.lode != voto.lode):
+                    return True
         return False
+
+
+    def copy(self):
+        nuovo = Libretto()
+        # nuovo._voti = self._voti.copy()
+        for v in self._voti:
+            nuovo._voti.append(v.copy())
+        return nuovo
+
+    def crea_migliorato(self):
+        """
+        Crea una copia del libretto e "migliora" i voti esso presenti.
+        :return:
+        """
+        nuovo = self.copy()
+        # nuovo._voti[0]    self._voti[0]
+
+        for v in nuovo._voti:
+            if 18<= v.punteggio <= 23:
+                v.punteggio += 1
+            elif 24<=v.punteggio<=28:
+                v.punteggio += 2
+            elif v.punteggio == 29:
+                v.punteggio = 30
+
+        return nuovo
+
+    def crea_ordinato_per_esame(self):
+        nuovo = self.copy()
+        # ordina i nuovo._voti
+        nuovo.ordina_per_esame()
+        return nuovo
+
+    def ordina_per_esame(self):
+        # ordina self._voti per nome esame
+        # self._voti.sort(key=estrai_campo_esame)
+        self._voti.sort(key=operator.attrgetter('esame'))
+        # self._voti.sort(key=lambda v: v.esame)
+        # self._voti.sort()
+
+    def crea_ordinato_per_punteggio(self):
+        nuovo = self.copy()
+        self._voti.sort(key=lambda v: (v.punteggio, v.lode), reverse=True)
+        return nuovo
+
+    def stampa(self):
+        print(f"Hai {len(self._voti)} voti")
+        for v in self._voti:
+            print(v)
+        print(f"La media vale {self.media():.2f}")
+
+    def cancella_inferiori(self,punteggio):
+    #     for v in self._voti:
+    #         if v.punteggio < punteggio:
+    #             self._voti.remove(v)
+    #
+    #     for i in range(len(self._voti)):
+    #         if self._voti[i] < punteggio:
+    #             self._voti.pop(i)
+    # #   [18,  30, 30, 30 ]
+
+        """
+        voti_nuovi = []
+        for v in self._voti:
+            if v.punteggio >= punteggio:
+                voti_nuovi.append(v)
+        self._voti = voti_nuovi
+        """
+
+        self._voti = [ v for v in self._voti if v.punteggio >= punteggio ]
+
+
+"""
+Opzione 1:
+metodo stampa_per_nome e metodo stampa_per_punteggio, che semplicemente stampano e non modificano nulla
+
+Opzione 2:
+metodo crea_libretto_ordinato_per_nome, ed un metodo crea_libretto_ordinato_per_punteggio, che creano
+delle copie separate, sulle quali potrò chiamar il metodo stampa()
+
+Opzione 3:
+metodo ordina_per_nome, che modifica il libretto stesso riordinando i Voti, e ordina_per_punteggio, poi userò
+stampa()
++ aggiungiamo gratis un metodo copy()
+
+Opzione 2bis:
+crea una copia shallow del libretto
+
+"""
+
+
+def _test_voto():
+    print(__name__)
+    v1 = Voto("nome esame", 8, 28, False, '2024-03-11')
+    l1 = Libretto()
+    l1.append(v1)
+    print(l1.media())
+
+if __name__ == "__main__":
+    _test_voto()
 
 def _test_voto():
     print(__name__)
